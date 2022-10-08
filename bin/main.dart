adca4d5e71689d255c0a942a62a7417f201d901a
williambulin <williambulin01@gmail.com>
2022-10-08 14:42:49 +0200

    add: new badge owner

--- bin/main.dart
+++ bin/main.dart
@@ -445,6 +445,9 @@ var users = <User>[
   User(name: 'cupppcakes', id: '148050043', badges: [
     UserBadge(badgeName: 'patreon_tier1'), // , acquiredAt: DateTime.now(), updatedAt: DateTime.now()),
   ]),
+  User(name: 'zomballr', id: '86173302', badges: [
+    UserBadge(badgeName: 'patreon_tier1'), // , acquiredAt: DateTime.now(), updatedAt: DateTime.now()),
+  ]),
 ];
 
 // // Future<List<Badge>> getBadges() async {
